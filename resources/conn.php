af82f62891181eb31b9fe3610ba767bd1bb4c553
bnaveenbharathi <naveenbharathi5050@gmail.com>
2025-02-18 09:09:36 +0530

    con change

--- resources/conn.php
+++ resources/conn.php
@@ -2,10 +2,15 @@
 
 $name = "GNT Research Health Care Center";
 
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db = 'gnt';
+// $host = 'localhost';
+// $user = 'root';
+// $pass = '';
+// $db = 'gnt';
+
+$host = "localhost";
+$user = "vattava2_health"; 
+$pass = "nbv@2022"; 
+$db = "vattava2_health";
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
